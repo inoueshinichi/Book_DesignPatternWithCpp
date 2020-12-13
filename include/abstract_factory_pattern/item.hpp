b2e29d9900accd70b1c05515e95ac526c64bd8b9
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2020-12-13 18:08:07 +0900

    Itemクラス追加

---
+++ include/abstract_factory_pattern/item.hpp
@@ -0,0 +1,20 @@
+#ifndef ABSTRACT_FACTORY_ITEM_HPP
+#define ABSTRACT_FACTORY_ITEM_HPP
+
+#include <string>
+
+namespace Is
+{
+    using std::string;
+    
+    class Item
+    {
+        string caption_;
+
+    public:
+        explicit Item(const string& caption) : caption_(caption) {}
+        virtual ~Item() {}
+        virtual string makeHTML() = 0;
+    };
+}
+#endif
