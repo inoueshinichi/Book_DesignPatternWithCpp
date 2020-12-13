8383553a9d462dd70673ab3351f06ad2e31bc51d
Inoue Shinichi XPS13 9310 on Ubuntu 20.04 LTS <inoue.shinichi.1800@gmail.com>
2020-12-13 18:08:44 +0900

    Trayクラス 追加

---
+++ include/abstract_factory_pattern/tray.hpp
@@ -0,0 +1,31 @@
+#ifndef ABSTRACT_FACTORY_TRAY_HPP
+#define ABSTRACT_FACTORY_TRAY_HPP
+
+#include "item.hpp"
+
+#include <memory>
+#include <string>
+#include <list>
+
+namespace Is
+{
+    using std::shared_ptr;
+    using std::string;
+    using std::list;
+
+    class Tray : public Item
+    {
+    protected:
+        list<shared_ptr<Item>> tray_;
+
+    public:
+        explicit Tray(const string& caption) : Item(caption) {}
+        virtual ~Tray() {}
+
+        void add(shared_ptr<Item> item)
+        {
+            tray_.push_back(item);
+        }
+    };
+}
+#endif
